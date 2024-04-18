211155b021af4272edfd5f9852cb220d7448e3df
DanielBaudryYounup <d.baudry@younup.fr>
2024-04-18 11:34:40 +0200

    Application first version

---
+++ src/business.py
@@ -0,0 +1,14 @@
+class Product:
+    def __init__(self, name: str, price: int):
+        self.name = name
+        self.price = price
+
+
+class Client:
+    def __init__(self, name: str, wallet: int):
+        self.name = name
+        self.wallet = wallet
+
+    def buy_product(self, product: Product):
+        if self.wallet > product.price:
+            print("Produit acheté !")
---
+++ src/main.py
@@ -0,0 +1,31 @@
+from business import Client, Product
+
+
+def main():
+    product1 = Product("Produit 1", 60)
+    product2 = Product("Produit 2", 100)
+    available_products = [product1, product2]
+
+    firstname = input("Quel est votre prénom ?")
+    client = Client(firstname, 200)
+    print(f"Bonjour {firstname}, votre solde est de : {client.wallet}\n")
+
+    client_is_online = True
+
+    while client_is_online:
+        print("Que voulez-vous acheter ?\n")
+        for product in available_products:
+            print(f"1) {product.name} ({product.price}€)\n")
+        print("0) Quitter\n")
+        choice = input("Votre choix ?\n")
+
+        if choice == "0":
+            client_is_online = False
+        elif choice == "1":
+            client.buy_product(product1)
+        elif choice == "2":
+            client.buy_product(product2)
+
+
+if __name__ == '__main__':
+    main()
